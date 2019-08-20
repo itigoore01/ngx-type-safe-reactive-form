3d5b68ca9cdaef1da5022c9d3b8319ee9318ba64
itigoore01 <xyz.shotao@gmail.com>
2019-08-20 15:08:41 +0900

    ci: Set up ci.

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -eu
+npm test -- ngx-type-safe-reactive-form --watch false --browsers ChromeHeadless
+npm run build -- ngx-type-safe-reactive-form
+cp ./README.md ./dist/ngx-type-safe-reactive-form
